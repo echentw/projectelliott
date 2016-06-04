a8731a45df775230980a3afcecb7e31e2da252ee
wxm3333 <m89122@gmail.com>
2016-06-04 15:31:42 -0400

    add title

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ProjectElliott' });
 });
 
 router.get('/progress', function(req, res) {
-  res.render('progress');
+  res.render('progress', { title: 'ProjectElliott' });
 });
 
 module.exports = router;
