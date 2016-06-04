949fb904ad25041e3ef0efa6a626ccd293c797d9
Eric Chen <echentw@mit.edu>
2016-06-04 14:56:31 -0400

    add problem submission logic

--- routes/problem.js
+++ routes/problem.js
@@ -5,11 +5,14 @@ var router = express.Router();
 
 var User = schemas.User;
 
+var TITLE = 'ProjectElliott';
+
 /* GET problems page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: TITLE });
 });
 
+/* GET specific problem */
 router.get('/:problem', function(req, res) {
   var index = req.params.problem;
   var json = fs.readFileSync('problems.json', 'utf8');
@@ -19,9 +22,51 @@ router.get('/:problem', function(req, res) {
     User.findOne({ 'username': 'Elliott' }, function(err, user) {
       var hasSolved = (user.problems.indexOf(Number(index)) != -1);
       var problemStatement = problems[index].problem;
-      res.render('problem', { hasSolved: hasSolved,
+      res.render('problem', { title: TITLE,
+                              hasSolved: hasSolved,
+                              index: index,
                               problemStatement: problemStatement });
     });
+  }
+});
+
+/* POST specific problem */
+router.post('/:problem', function(req, res) {
+  var index = req.params.problem;
+  var json = fs.readFileSync('problems.json', 'utf8');
+  var problems = JSON.parse(json);
+
+  var submittedAnswer = Number(req.body.submittedAnswer);
+
+  if (index in problems) {
+    if (problems[index].answer == submittedAnswer) {
+      // handle correct answer
+      User.findOne({ 'username': 'Elliott' }, function(err, user) {
+        var answer = problems[index].answer;
+        var hasSolved = (user.problems.indexOf(Number(index)) != -1);
+
+        if (!hasSolved) {
+          // if problem hasn't been solved yet, append it to the user array
+          User.update({ 'username': 'Elliott' },
+                      { $push: { 'problems': Number(index) } },
+                      function(err) {
+            res.render('problem', { title: TITLE,
+                                    hasSolved: hasSolved,
+                                    index: index });
+          });
+        } else {
+          // if the problem has been solved already...
+          res.render('problem', { title: TITLE,
+                                  hasSolved: hasSolved,
+                                  index: index });
+        }
+      });
+    } else {
+      // handle incorrect answer
+      res.render('incorrect', { title: TITLE });
+    }
+  } else {
+    res.render('error');
   }
 });
 
