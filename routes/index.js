88164c690dc60a68d1ecc87ba45b0bfa7708545a
Eric Chen <echentw@mit.edu>
2016-06-09 22:46:49 -0400

    links and progress on homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var fs = require('fs');
+var schemas = require('../models/schemas');
 var router = express.Router();
+
+var User = schemas.User;
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'ProjectElliott' });
+  var json = fs.readFileSync('problems.json', 'utf8');
+  var problems = JSON.parse(json);
+
+  User.findOne({ 'username': 'Elliott' }, function(err, user) {
+    var progress = [];
+    var counter = 1;
+    for (index in problems) {
+      if (user.problems.indexOf(Number(index)) != -1) {
+        progress.push({ number: counter, solved: true });
+      } else {
+        progress.push({ number: counter, solved: false });
+      }
+      ++counter;
+    }
+
+    res.render('index', { title: 'ProjectElliott',
+                          progress: progress });
+  });
 });
 
 module.exports = router;
