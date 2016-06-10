57552fd6af4d461df708a6f84576f3ba64bd7186
Eric Chen <echentw@mit.edu>
2016-06-09 23:50:09 -0400

    implement progress page skeleton

--- routes/progress.js
+++ routes/progress.js
@@ -1,9 +1,36 @@
 var express = require('express');
+var fs = require('fs');
+var schemas = require('../models/schemas');
 var router = express.Router();
+
+var User = schemas.User;
 
 /* GET progress page */
 router.get('/', function(req, res) {
-  res.render('progress', { title: 'ProjectElliott' });
+  var json = fs.readFileSync('problems.json', 'utf8');
+  var problems = JSON.parse(json);
+
+  User.findOne({ 'username': 'Elliott' }, function(err, user) {
+    var progress = [];
+    var counter = 1;
+    var numSolved = 0;
+    for (index in problems) {
+      if (user.problems.indexOf(Number(index)) != -1) {
+        progress.push({ number: counter, solved: true });
+        ++numSolved;
+      } else {
+        progress.push({ number: counter, solved: false });
+      }
+      ++counter;
+    }
+
+    var oneSolved = (numSolved == 1);
+
+    res.render('progress', { title: 'ProjectElliott',
+                             progress: progress,
+                             numSolved: numSolved,
+                             oneSolved: oneSolved });
+  });
 });
 
 module.exports = router;
