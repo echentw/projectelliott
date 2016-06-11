3569010f461bf39b6f1abf2f6ce0a5de38b40bb4
Eric Chen <echentw@mit.edu>
2016-06-11 12:25:02 -0400

    autosave in DB

--- routes/index.js
+++ routes/index.js
@@ -5,28 +5,52 @@ var router = express.Router();
 
 var User = schemas.User;
 
+/*
+ * Check if Elliott is in the DB.
+ * If not, save it.
+ */
+checkDB = function(username, cb) {
+  User.findOne({ 'username': username }, function(err, user) {
+    if (!user) {
+      console.log("First time user! Creating user.");
+      var user = new User({
+        username: 'Elliott',
+        problems: []
+      });
+      user.save(function (err) {
+        cb();
+      });
+    } else {
+      console.log("Elliott has already been created!");
+      cb();
+    }
+  });
+};
+
 /* GET home page. */
 router.get('/', function(req, res) {
   var json = fs.readFileSync('problems.json', 'utf8');
   var problems = JSON.parse(json);
 
-  User.findOne({ 'username': 'Elliott' }, function(err, user) {
-    var progress = [];
-    var counter = 0;
-    var numSolved = 0;
-    for (index in problems) {
-      ++counter;
-      if (user.problems.indexOf(Number(index)) != -1) {
-        progress.push({ number: counter, solved: true });
-        ++numSolved;
-      } else {
-        progress.push({ number: counter, solved: false });
+  checkDB('Elliott', function() {
+    User.findOne({ 'username': 'Elliott' }, function(err, user) {
+      var progress = [];
+      var counter = 0;
+      var numSolved = 0;
+      for (index in problems) {
+        ++counter;
+        if (user.problems.indexOf(Number(index)) != -1) {
+          progress.push({ number: counter, solved: true });
+          ++numSolved;
+        } else {
+          progress.push({ number: counter, solved: false });
+        }
       }
-    }
 
-    res.render('index', { title: 'ProjectElliott',
-                          progress: progress,
-                          numSolved: numSolved });
+      res.render('index', { title: 'ProjectElliott',
+                            progress: progress,
+                            numSolved: numSolved });
+    });
   });
 });
 
